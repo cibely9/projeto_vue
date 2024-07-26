d24d711ce4166600b557708a0b67cc492553362d
cibely9 <161332785+cibely9@users.noreply.github.com>
2024-07-26 09:05:42 -0300

    Add files via upload

--- src/components/PaginaPrincipal.vue
+++ src/components/PaginaPrincipal.vue
@@ -1,5 +1,38 @@
+<script setup>
+
+import TabelaComponent from './TabelaComponent.vue';
+
+</script>
+
 <template>
-   <ImagemPrincipal/>
+  <div class="hello">
+    <header>
+      <div id="conteudo-principal">
+        <h1>Turma da Mônica</h1>
+        <p>
+          Turma da Mônica é uma série de histórias em quadrinhos e uma franquia de mídia produzida pela Maurício de
+          Sousa Produções, criada pelo cartunista e empresário Mauricio de Sousa.
+        </p>
+      </div>
+      <div>
+        <img src="@/assets/monica.png" alt="monica.png" width="43%">
+      </div>
+      <div>
+        <img src="@/assets/turma.png" alt="turma.png" width="90%">
+      </div>
+      <div>
+        <img src="@/assets/monica2.png" alt="monica2.png" width="50%">
+      </div>
+      <br>
+      <br>
+      <br>
+      <iframe width="976" height="549" src="https://www.youtube.com/embed/zKUpkxP94_U"
+        title="Brincadeiras Modernas | Turma da Mônica" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    </header>
+    <TabelaComponent/>
+  </div>
 </template>
 
 <script>
@@ -18,6 +51,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/TabelaComponent.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'TabelaComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Caracteristica</th>
+        <th>Imagem</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Monica</td>
+        <td>Tem um coelho azul</td>
+        <td class="monica"><img src="@/assets/monica100.png" width="20%" alt="monica"></td>
+      </tr>
+      <tr>
+        <td>Cebolinha</td>
+        <td>Elabora planos</td>
+        <td class="cebolinha"><img src="@/assets/Cebolinha.png" width="20%" alt="cebolinha"></td>
+      </tr>
+      <tr>
+        <td>Magali</td>
+        <td>Come muito</td>
+        <td class="magali"><img src="@/assets/magali.png" width="20%" alt="magali"></td>
+      </tr>
+      <tr>
+        <td>Cascão</td>
+        <td>Não toma banho</td>
+        <td class="cascao"><img src="@/assets/Cascao.webp" width="20%" alt="cascao"></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
